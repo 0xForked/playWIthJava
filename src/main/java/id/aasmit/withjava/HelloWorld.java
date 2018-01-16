38e8c497e9ad6c357049c68b2b31f18ea42fad4a
aasumitro <aasumitro@merahputih.id>
2018-01-16 09:13:12 +0800

    Initial commit

---
+++ src/main/java/id/aasmit/withjava/HelloWorld.java
@@ -0,0 +1,27 @@
+package id.aasmit.withjava;
+
+public class HelloWorld {
+
+    //Comment with java - Comment single line
+
+    /* Comment v1
+     * asd qwe
+     * zxc asd
+     */
+
+    /**
+     * Comment v2
+     * @Title Main function
+     * @Description Bla bla
+     * @param args
+     */
+
+    public static void main(String[] args){
+
+        System.out.print("Hello World");
+
+    }
+
+
+
+}
